6bb1a9c96a262aa7e826b056b2fefd91b9240dfe
jkenneth24 <jkgerero@sorsu.edu.ph>
2025-09-14 10:34:23 +0800

    migration and model for article management

---
+++ app/Models/Article.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title_article',
+        'thumbnail_image',
+        'article_content',
+        'date_written',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
